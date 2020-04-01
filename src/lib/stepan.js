c1ac53a26bb2ed5867dcfe8352cc429192ad6090
solka13Lenio <solomialenio13@gmail.com>
2020-04-01 12:32:46 +0300

    stepan.js todo

--- src/lib/stepan.js
+++ src/lib/stepan.js
@@ -1,36 +1,53 @@
+import StepanError from './stepanError.js'
 
 export default class Stepan {
+  static isValidTagName(tagName) {
+      const validTagNames = "a abbr acronym address applet area article aside audio b base basefont bdi bdo big blockquote body br button canvas caption center cite code col colgroup data datalist dd del details dfn dialog dir div dl dt em embed fieldset figcaption figure font footer form frame frameset h1 h2 h3 h4 h5 h6 header hr html i iframe img input ins kbd label legend li link main map mark meta meter nav noframes noscript ol optgroup option output p param picture pre progress q rp rt ruby s samp script section select small source span strike strong style sub summary sup svg table tbody td template textarea tfoot th thead time title tr track tt u ul var video wbr"
+      if (validTagNames.indexOf(tagName) != -1) 
+        return true;
+      return false;
+  }
+
   static createElement(element, parent, attributes = {}) {
-    // TODO: check if this is a valid tag name
-    const newElement = document.createElement(element);
+    if (!this.isValidTagName(element)) {
+        throw new StepanError("! invalid tag name");
+    }
+    else {
+      const newElement = document.createElement(element);
 
-    const { innerHTML, innerText } = attributes;
+      const { innerHTML, innerText } = attributes;
 
-    for (let attribute in attributes) {
-      if (['innerHTML', 'innerText'].includes(attribute)) {
-        continue;
+      for (let attribute in attributes) {
+        if (['innerHTML', 'innerText'].includes(attribute)) {
+          continue;
+        }
+
+        newElement.setAttribute(attribute, attributes[attribute]);
       }
 
-      newElement.setAttribute(attribute, attributes[attribute]);
+      innerHTML && (newElement.innerHTML = innerHTML);
+      innerText && (newElement.innerText = innerText);
+
+      parent.appendChild(newElement);
+
+      return newElement;
     }
-
-    innerHTML && (newElement.innerHTML = innerHTML);
-    innerText && (newElement.innerText = innerText);
-
-    parent.appendChild(newElement);
-
-    return newElement;
   }
 
   static Component = class {
     constructor(parent) {
-
-      // TODO: 1. Create StepanError class to define all framework errors
-      //       2. throw an error if parent is null or undefined, or if it's not a valid DOM object
-
+      if (typeof(parent) === "null" || typeof(parent) === "undefined") {
+        let parentType = "null";
+        if (typeof(parent) === "undefined") 
+          parentType = "undefined";
+        throw new StepanError("! " + parentType + " parent element");
+      }
+      if (! parent instanceof HTMLElement) {
+        throw new StepanError("! invalid DOM object");
+      }
       this.parent = parent;
     }
-
+    
     // TODO (Bonus): Ensure that every component returns a top-level root element
   }
 }
---
+++ src/lib/stepanError.js
@@ -0,0 +1,7 @@
+export default class StepanError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "StepanError";
+    }
+}
+
